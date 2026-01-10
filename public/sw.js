69a14060d8a16cabf9c802d8f60acac15e4d82d2
Linus Hallerström <hallerstrom.linus@gmail.com>
2026-01-10 18:57:49 +0100

    push

--- public/sw.js
+++ public/sw.js
@@ -1,2 +1,8 @@
-self.addEventListener('fetch', () => {
+self.addEventListener('install', (event) => {
+  console.log('Service Worker installerad');
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', (event) => {
+  // Behövs för att Chrome ska klassa det som en installerbar PWA
 });
